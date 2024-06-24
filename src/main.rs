e644f8bb5f7e0e2f5d17588d346d908a042fd14d
Arkitu <85173315+Arkitu@users.noreply.github.com>
2024-06-24 09:11:13 +0200

    save

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
-use std::f32::consts::E;
+use std::array;
+use rand::prelude::*;
+
 type F = f32;
+const E: F = std::f32::consts::E;
 
 fn sigmoid(x: F) -> F {
     1. / (1. + E.powf(-x))
 }
 
+#[derive(Debug)]
 struct Node {
     bias: F,
     sources: Vec<(F, usize)>
 }
 
+#[derive(Debug)]
 struct Nn<const IN: usize, const OUT: usize> {
     nodes: Vec<Node>
 }
@@ -26,21 +31,25 @@ impl<const IN: usize, const OUT: usize> Nn<IN, OUT> {
             }
             vals.push(sigmoid(v));
         }
-        vals[vals.len()-OUT-1..].try_into().unwrap()
+        vals[vals.len()-OUT..].try_into().unwrap()
     }
 }
 
+#[derive(Debug, Clone, Copy)]
 struct Neuron {
     evo_num: usize,
     bias: F
 }
+#[derive(Debug, Clone)]
 struct Link {
     evo_num: usize,
     strength: F,
     in_neuron: usize, // evo_num
-    out_neuron: usize // evo_num
+    out_neuron: usize, // evo_num
+    disabled: bool
 }
 
+#[derive(Debug)]
 enum Gene {
     Neuron(Neuron),
     Link(Link)
@@ -54,6 +63,7 @@ impl Gene {
     }
 }
 
+#[derive(Debug)]
 struct Phenotype<const IN: usize, const OUT: usize> {
     neurons: Vec<Neuron>, // Keep in order
     links: Vec<Link>
@@ -85,11 +95,56 @@ impl<const IN: usize, const OUT: usize> Phenotype<IN, OUT> {
             nodes
         }
     }
+    fn mutate(&mut self, current_evo_num: &mut usize) {
+        let mut rng = thread_rng();
+        for n in self.neurons.iter_mut() {
+            n.bias = (n.bias + rng.gen_range(-0.3..0.3)).min(1.);
+        }
+        for l in self.links.iter_mut() {
+            l.strength = (l.strength + rng.gen_range(-0.3..0.3)).min(1.);
+        }
+        if self.links.len() > 0 && rng.gen_bool(0.1) {
+            let l = self.links.choose_mut(&mut rng).unwrap();
+            l.disabled = true;
+            let l = l.clone();
+
+            let ns = self.neurons.iter().enumerate().filter(|(_, n)| n.evo_num == l.in_neuron || n.evo_num == l.evo_num).map(|(i,n)| (i,n.evo_num)).collect::<Vec<_>>();
+            assert_eq!(ns.len(), 2);
+
+            
+            self.neurons.insert(rng.gen_range((ns[0].0+1)..=ns[1].0), Neuron {
+                evo_num: *current_evo_num+1,
+                bias: 0.
+            });
+
+            self.links.push(Link {
+                evo_num: *current_evo_num+2,
+                strength: l.strength.abs().sqrt(),
+                in_neuron: ns[0].1,
+                out_neuron: *current_evo_num+1,
+                disabled: false
+            });
+            self.links.push(Link {
+                evo_num: *current_evo_num+3,
+                strength: l.strength.abs().sqrt().copysign(l.strength),
+                in_neuron: *current_evo_num+1,
+                out_neuron: ns[1].1,
+                disabled: false
+            });
+
+            *current_evo_num += 3;
+        }
+        if rng.gen_bool(0.2) {
+            let n1 = self.neurons.iter().enumerate().choose(&mut rng).unwrap();
+            
+        }
+    }
 }
 
+#[derive(Debug)]
 struct Trainer<const IN: usize, const OUT: usize, const GENERATION_SIZE: usize = 50> {
     score_fn: fn([Nn<IN, OUT>; GENERATION_SIZE]) -> [F; GENERATION_SIZE],
-    current_gen: Option<[Phenotype<IN, OUT>; GENERATION_SIZE]>,
+    current_gen: Option<[(Phenotype<IN, OUT>, F); GENERATION_SIZE]>,
     current_evo_num: usize
 }
 impl<const IN: usize, const OUT: usize, const GENERATION_SIZE: usize> Trainer<IN, OUT, GENERATION_SIZE> {
@@ -101,12 +156,33 @@ impl<const IN: usize, const OUT: usize, const GENERATION_SIZE: usize> Trainer<IN
         }
     }
     fn train(&mut self) {
-        let gen = self.current_gen.unwrap_or([]);
+        let mut gen = self.current_gen.take().unwrap_or_else(|| array::from_fn(|_| (Phenotype::default(), 0.)));
+        for p in gen.iter_mut() {
+            p.0.mutate(&mut self.current_evo_num);
+        }
+        let scores = (self.score_fn)(array::from_fn(|i| gen[i].0.nn()));
+        for (i, s) in scores.into_iter().enumerate() {
+            gen[i].1 = s;
+        }
+        gen.sort_unstable_by(|a,b| a.1.total_cmp(&b.1).reverse());
+        self.current_gen = Some(gen);
+    }
+    fn best_score(&self) -> Option<F> {
+        self.current_gen.as_ref().map(|g| g[0].1)
     }
 }
 
 fn main() {
-    let trainer = Trainer::<2, 1>::new(|gen| gen.map(|nn| {
-        dbg!(-nn.eval([0., 0.])[0]+nn.eval([1., 0.])[0]+nn.eval([0., 1.])[0]-nn.eval([1., 1.])[0])
+    // let p = Phenotype {
+    //     neurons: vec![Neuron]
+    // }
+
+    let mut trainer = Trainer::<2, 1>::new(|gen| gen.map(|nn| {
+        dbg!(&nn);
+        dbg!(-nn.eval([0., 0.])[0])+dbg!(nn.eval([1., 0.])[0])+dbg!(nn.eval([0., 1.])[0])-dbg!(nn.eval([1., 1.])[0])
     }));
+    for _ in 0..10 {
+        dbg!(trainer.best_score());
+        trainer.train();
+    }
 }
