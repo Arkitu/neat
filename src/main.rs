e40362c0b85226d75d63c12c1ddda5573cb5c5fc
Arkitu <85173315+Arkitu@users.noreply.github.com>
2024-06-25 22:09:44 +0200

    save

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-use std::array;
-use rand::prelude::*;
+use std::{array, collections::HashMap, sync::{Arc, RwLock}};
+use rand::{distributions::Uniform, prelude::*};
+use plotters::prelude::*;
+use rayon::prelude::*;
 
 type F = f32;
 const E: F = std::f32::consts::E;
@@ -63,7 +65,7 @@ impl Gene {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Phenotype<const IN: usize, const OUT: usize> {
     neurons: Vec<Neuron>, // Keep in order
     links: Vec<Link>
@@ -95,49 +97,140 @@ impl<const IN: usize, const OUT: usize> Phenotype<IN, OUT> {
             nodes
         }
     }
-    fn mutate(&mut self, current_evo_num: &mut usize) {
+    fn mutate(&mut self, cen: &mut usize) {
         let mut rng = thread_rng();
-        for n in self.neurons.iter_mut() {
-            n.bias = (n.bias + rng.gen_range(-0.3..0.3)).min(1.);
+        // self.neurons.par_iter_mut().for_each_init(|| thread_rng(), |rng, n| n.bias += rng.gen_range(-0.1..0.1));
+        // self.links.par_iter_mut().for_each_init(|| thread_rng(), |rng, l| l.strength += rng.gen_range(-0.1..0.1));
+        for (n, r) in self.neurons.iter_mut().zip(Uniform::new(-0.1, 0.1).sample_iter(rng.clone())) {
+            n.bias = (n.bias+r).clamp(-2., 2.);
         }
-        for l in self.links.iter_mut() {
-            l.strength = (l.strength + rng.gen_range(-0.3..0.3)).min(1.);
+        for (l, r) in self.links.iter_mut().zip(Uniform::new(-0.1, 0.1).sample_iter(rng.clone())) {
+            l.strength = (l.strength+r).clamp(-2., 2.);
         }
-        if self.links.len() > 0 && rng.gen_bool(0.1) {
+        if self.links.len() > 0 && rng.gen_bool(0.03) {
             let l = self.links.choose_mut(&mut rng).unwrap();
             l.disabled = true;
             let l = l.clone();
 
-            let ns = self.neurons.iter().enumerate().filter(|(_, n)| n.evo_num == l.in_neuron || n.evo_num == l.evo_num).map(|(i,n)| (i,n.evo_num)).collect::<Vec<_>>();
-            assert_eq!(ns.len(), 2);
+            let ns = self.neurons.iter().enumerate().filter(|(_, n)| n.evo_num == l.in_neuron || n.evo_num == l.out_neuron).map(|(i,n)| (i,n.evo_num)).collect::<Vec<_>>();
 
-            
-            self.neurons.insert(rng.gen_range((ns[0].0+1)..=ns[1].0), Neuron {
-                evo_num: *current_evo_num+1,
+            self.neurons.insert(rng.gen_range((ns[0].0.max(IN-1)+1)..=ns[1].0.min(self.neurons.len()+1-OUT)), Neuron {
+                evo_num: *cen+1,
                 bias: 0.
             });
 
             self.links.push(Link {
-                evo_num: *current_evo_num+2,
+                evo_num: *cen+2,
                 strength: l.strength.abs().sqrt(),
                 in_neuron: ns[0].1,
-                out_neuron: *current_evo_num+1,
+                out_neuron: *cen+1,
                 disabled: false
             });
             self.links.push(Link {
-                evo_num: *current_evo_num+3,
+                evo_num: *cen+3,
                 strength: l.strength.abs().sqrt().copysign(l.strength),
-                in_neuron: *current_evo_num+1,
+                in_neuron: *cen+1,
                 out_neuron: ns[1].1,
                 disabled: false
             });
 
-            *current_evo_num += 3;
+            *cen += 3;
         }
-        if rng.gen_bool(0.2) {
-            let n1 = self.neurons.iter().enumerate().choose(&mut rng).unwrap();
-            
+        if rng.gen_bool(0.05) {
+            let i1 = rng.gen_range(0..(self.neurons.len()-OUT));
+            let i2 = rng.gen_range((i1+1).max(IN)..self.neurons.len());
+            let e1 = self.neurons[i1].evo_num;
+            let e2 = self.neurons[i2].evo_num;
+            if !self.links.iter().any(|l| l.in_neuron == e1 && l.out_neuron == e2) {
+                self.links.push(Link {
+                    evo_num: *cen+1,
+                    strength: 0.,
+                    in_neuron: e1,
+                    out_neuron: e2,
+                    disabled: false
+                });
+                *cen += 1;
+            }
         }
+        if self.links.len() > 0 && rng.gen_bool(0.2) {
+            self.links.remove(rng.gen_range(0..self.links.len()));
+        }
+        if self.neurons.len() > IN+OUT && rng.gen_bool(0.2) {
+            let n = self.neurons.remove(rng.gen_range((IN+1)..=(self.neurons.len()-OUT)));
+            let mut r = 0;
+            for i in 0..self.links.len() {
+                let l = &self.links[i-r];
+                if l.in_neuron == n.evo_num || l.out_neuron == n.evo_num {
+                    self.links.remove(i-r);
+                    r += 1;
+                }
+            }
+        }
+    }
+    fn mix(&self, other: &Self) -> Self {
+        let mut neurons = self.neurons.clone();
+        for n in neurons.iter_mut() {
+            if let Some(on) = other.neurons.iter().find(|on| on.evo_num == n.evo_num) {
+                n.bias = (n.bias + on.bias) / 2.;
+            }
+        }
+        let mut links = self.links.clone();
+        for l in links.iter_mut() {
+            if let Some(ol) = other.links.iter().find(|ol| ol.evo_num == l.evo_num) {
+                l.strength = (l.strength + ol.strength) / 2.;
+            }
+        }
+        Self {
+            neurons,
+            links
+        }
+    }
+    fn draw(&self, file_name: &str) {
+        use draw::*;
+
+        let mut canvas = Canvas::new(1000, 1000);
+
+        let mut rng = thread_rng();
+        let mut neurons = HashMap::with_capacity(self.neurons.len());
+        for (i,n) in self.neurons.iter().enumerate() {
+            let pos = Point::new((i.min(self.neurons.len()-OUT).saturating_sub(IN-1) as f32) * 1000. / (self.neurons.len() as f32), rng.gen_range(0. ..1000.));
+            canvas.display_list.add(
+                Drawing::new()
+                    .with_shape(Shape::Circle { radius: 4 })
+                    .with_position(pos)
+                    .with_style(Style::filled(Color::black()))
+            );
+            neurons.insert(n.evo_num, pos);
+        }
+
+        for l in self.links.iter() {
+            let point1 = neurons.get(&l.in_neuron).unwrap();
+            let point2 = neurons.get(&l.out_neuron).unwrap();
+            canvas.display_list.add(
+                Drawing::new()
+                    .with_shape(LineBuilder::new(point1.x, point1.y).line_to(point2.x, point2.y).build())
+                    .with_style(Style::stroked((l.strength.abs()*3.) as u32, RGB::new((-l.strength*156.) as u8, (l.strength*156.) as u8, 0)))
+            );
+        }
+
+        // // create a new drawing
+        // let mut rect = Drawing::new()
+        //     // give it a shape
+        //     .with_shape(Shape::Line { start: , points: () })
+        //     // move it around
+        //     .with_xy(25.0, 25.0)
+        //     // give it a cool style
+        //     .with_style(Style::stroked(5, Color::black()));
+
+        // // add it to the canvas
+        // canvas.display_list.add(rect);
+
+        // save the canvas as an svg
+        render::save(
+            &canvas,
+            file_name,
+            SvgRenderer::new(),
+        ).expect("Failed to save");
     }
 }
 
@@ -155,8 +248,20 @@ impl<const IN: usize, const OUT: usize, const GENERATION_SIZE: usize> Trainer<IN
             current_evo_num: 0
         }
     }
-    fn train(&mut self) {
+    fn train(&mut self, turnover: F) {
         let mut gen = self.current_gen.take().unwrap_or_else(|| array::from_fn(|_| (Phenotype::default(), 0.)));
+        let mut random_ns = gen.choose_multiple(&mut thread_rng(), (GENERATION_SIZE as F * turnover * 2.) as usize);
+        let mut children = Vec::with_capacity((GENERATION_SIZE as F * turnover) as usize);
+        while let Some(((n1, s1), (n2, s2))) = random_ns.next().and_then(|x1| random_ns.next().map(|x2| (x1, x2))) {
+            if s1 > s2 {
+                children.push(n1.mix(n2));
+            } else {
+                children.push(n2.mix(n1));
+            }
+        }
+        for (i, c) in children.into_iter().enumerate() {
+            gen[gen.len()-1-i] = (c, 0.);
+        }
         for p in gen.iter_mut() {
             p.0.mutate(&mut self.current_evo_num);
         }
@@ -170,19 +275,41 @@ impl<const IN: usize, const OUT: usize, const GENERATION_SIZE: usize> Trainer<IN
     fn best_score(&self) -> Option<F> {
         self.current_gen.as_ref().map(|g| g[0].1)
     }
+    fn best(&self) -> Option<Phenotype<IN, OUT>> {
+        self.current_gen.as_ref().map(|g| g[0].0.clone())
+    }
 }
 
 fn main() {
-    // let p = Phenotype {
-    //     neurons: vec![Neuron]
-    // }
-
     let mut trainer = Trainer::<2, 1>::new(|gen| gen.map(|nn| {
-        dbg!(&nn);
-        dbg!(-nn.eval([0., 0.])[0])+dbg!(nn.eval([1., 0.])[0])+dbg!(nn.eval([0., 1.])[0])-dbg!(nn.eval([1., 1.])[0])
+        -nn.eval([0., 0.])[0]+nn.eval([1., 0.])[0]+nn.eval([0., 1.])[0]-nn.eval([1., 1.])[0]
     }));
-    for _ in 0..10 {
-        dbg!(trainer.best_score());
-        trainer.train();
+
+    let mut best_scores = Vec::new();
+    for _ in 0..10000 {
+        best_scores.push(trainer.best_score().unwrap_or_default());
+        trainer.train(0.5);
     }
+    let best = dbg!(trainer.best().unwrap());
+    let nn = best.nn();
+    best.draw("best.svg");
+    dbg!(nn.eval([0., 0.])[0], nn.eval([1., 0.])[0], nn.eval([0., 1.])[0], nn.eval([1., 1.])[0]);
+
+    let root_drawing_area = BitMapBackend::new("graph.png", (1024, 768))
+        .into_drawing_area();
+
+    root_drawing_area.fill(&WHITE).unwrap();
+
+    let mut chart = ChartBuilder::on(&root_drawing_area)
+        .set_label_area_size(LabelAreaPosition::Left, 40)
+        .set_label_area_size(LabelAreaPosition::Bottom, 40)
+        .build_cartesian_2d(0..best_scores.len(), best_scores.clone().into_iter().reduce(F::min).unwrap()..best_scores.clone().into_iter().reduce(F::max).unwrap())
+        .unwrap();
+
+    chart.configure_mesh().draw().unwrap();
+
+    chart.draw_series(LineSeries::new(
+        best_scores.into_iter().enumerate(),
+        &RED
+    )).unwrap();
 }
